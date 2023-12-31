2db19228683b2e3485dc00e42c40b3a5219b18a6
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-12-31 16:36:37 -0500

    fully functioning game, more styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isWinner }) {
 	const [isClicked, setIsClicked] = useState(false)
 
 	function squareIsClicked() {
@@ -16,8 +16,10 @@ function Square({ value, onSquareClick }) {
 		squareStyle = { backgroundColor: '#ffdd57', color: '#000000', cursor: 'initial' }
 	}
 
+	const squareButton = isWinner ? 'square-winner' : 'square'
+
 	return (
-		<button className="square" onClick={squareIsClicked} style={squareStyle}>
+		<button className={squareButton} onClick={squareIsClicked} style={squareStyle}>
 			{value}
 		</button>
 	)
@@ -29,7 +31,7 @@ export default function Board() {
 
 	function handleClick(index) {
 		// If square is already filled, return early.
-		if (squares[index]) {
+		if (squares[index] || calculateWinner(squares)) {
 			return
 		}
 
@@ -39,17 +41,68 @@ export default function Board() {
 		setXisNext(!xIsNext)
 	}
 
+	// Game status
+	const winner = calculateWinner(squares)
+	const isWinner = winner !== null
+	let status
+	let statusStyle = {}
+
+	if (winner) {
+		status = `Winner: ${winner}!`
+		statusStyle = { color: '#30ce3d' }
+	} else if (!squares.includes(null)) {
+		status = 'Tie Game!'
+		statusStyle = { color: '#f64c4c' }
+	} else {
+		status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+		statusStyle = { textShadow: 'none' }
+	}
+
+	function resetGame() {
+		setSquares(Array(9).fill(null))
+		setXisNext(true)
+	}
+
 	return (
-		<div className="board">
-			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-			<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-			<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-			<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-			<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-			<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-			<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-			<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-		</div>
+		<>
+			<div className="status" style={statusStyle}>
+				{status}
+			</div>
+			<div className="board">
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} isWinner={isWinner} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} isWinner={isWinner} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} isWinner={isWinner} />
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} isWinner={isWinner} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} isWinner={isWinner} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} isWinner={isWinner} />
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} isWinner={isWinner} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} isWinner={isWinner} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} isWinner={isWinner} />
+			</div>
+			<button className="reset" onClick={resetGame}>
+				Reset Game
+			</button>
+		</>
 	)
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	]
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i]
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a]
+		}
+	}
+	return null
 }
